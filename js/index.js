25307aa669fa0e9a08b6c08c610f2facd083ca81
Sathwik279 <ssathwik31831@gmail.com>
2024-07-18 08:30:37 +0530

    1st commit of the project

---
+++ js/index.js
@@ -0,0 +1,41 @@
+
+let images = []
+async function getImages(){
+  let a = await fetch('http://127.0.0.1:5501/images/')
+  let response = await a.text();
+  console.log(response)
+  let div = document.createElement('div');
+  div.innerHTML = response;
+  let im = div.getElementsByTagName('a')
+ 
+  for(let index=0;index<im.length;index++){
+    const element = im[index]
+    if(element.href.endsWith('.png'))
+    {
+      images.push(element.href)
+    }
+    
+  }
+  console.log(images)
+}
+
+getImages();
+
+const start = document.getElementById("start");
+let index = 0;
+start.addEventListener("click",()=>{
+
+  index++;
+  console.log(index);
+  
+    if(index<images.length){
+      console.log("image is successfully displayed")
+      const image1 = document.getElementById("image1");
+      image1.src = images[index];
+    }
+    else
+    {
+      console.log("displaying of image is unsuccessful")
+    }
+  })
+
